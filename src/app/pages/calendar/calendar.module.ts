5dbaf6bc71889aa0d7d49f407300bb7dae07ef69
alex <alexcarvajal2404@gmail.com>
2021-07-06 18:04:27 -0400

    implemented calendar first version

--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
 import { CalendarComponent } from './calendar.component';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
+import { MaterialModule } from '../../material/material.module';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin,
+]);
 
 @NgModule({
-  declarations: [],
+  declarations: [CalendarComponent],
   imports: [
+    CommonModule,
+    FullCalendarModule,
+    MaterialModule,
     RouterModule.forChild([{path: '', component: CalendarComponent}]),
-    CommonModule
   ]
 })
 export class CalendarModule { }
