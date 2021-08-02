d6fb073bcfac8a93f5253959dbe20c6bbf3249d8
alex <alexcarvajal2404@gmail.com>
2021-08-01 22:26:55 -0400

    integrated the view for registering clients to pools

--- src/app/pages/pools-client/pools-client.module.ts
+++ src/app/pages/pools-client/pools-client.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { ListOfPoolsComponent } from './list-of-pools/list-of-pools.component';
-import { PoolsResultsComponent } from './pools-results/pools-results.component';
-import { PoolCategoryComponent } from './pool-category/pool-category.component';
-import { PoolComponent } from './pool/pool.component';
-import { PoolsComponent } from './pools/pools.component';
-import { SharedModule } from '../../shared/shared.module';
-import { MaterialModule } from '../../material/material.module';
-import { PoolResultComponent } from './pool-result/pool-result.component';
-import { RegisterToPoolComponent } from './register-to-pool/register-to-pool.component';
-import {FormsModule} from "@angular/forms";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ListOfPoolsComponent} from './list-of-pools/list-of-pools.component';
+import {PoolsResultsComponent} from './pools-results/pools-results.component';
+import {PoolCategoryComponent} from './pool-category/pool-category.component';
+import {PoolComponent} from './pool/pool.component';
+import {PoolsComponent} from './pools/pools.component';
+import {SharedModule} from '../../shared/shared.module';
+import {MaterialModule} from '../../material/material.module';
+import {PoolResultComponent} from './pool-result/pool-result.component';
+import {RegisterToPoolComponent} from './register-to-pool/register-to-pool.component';
+import {FormsModule} from '@angular/forms';
+import {PoolRegisterComponent} from './pool-register/pool-register.component';
 
 @NgModule({
   declarations: [
@@ -20,44 +21,50 @@ import {FormsModule} from "@angular/forms";
     PoolComponent,
     PoolsComponent,
     PoolResultComponent,
-    RegisterToPoolComponent
+    RegisterToPoolComponent,
+    PoolRegisterComponent
   ],
-    imports: [
-        CommonModule,
-        SharedModule,
-        MaterialModule,
-        RouterModule.forChild([
-                {
-                    path: '',
-                    component: PoolsComponent
-                },
-                {
-                    path: 'list-of-pools',
-                    component: ListOfPoolsComponent
-                },
-                {
-                    path: 'pools-results',
-                    component: PoolsResultsComponent
-                },
-                {
-                    path: 'pool-category/:id',
-                    component: PoolCategoryComponent
-                },
-                {
-                    path: 'pool/:id',
-                    component: PoolComponent
-                },
-                {
-                    path: 'pool-result/:id',
-                    component: PoolResultComponent
-                },
-                {
-                    path: 'register-to-pool/:id',
-                    component: RegisterToPoolComponent
-                },
-            ]
-        ),
-        FormsModule
-    ]
+  imports: [
+    CommonModule,
+    SharedModule,
+    MaterialModule,
+    RouterModule.forChild([
+        {
+          path: '',
+          component: PoolsComponent
+        },
+        {
+          path: 'list-of-pools',
+          component: ListOfPoolsComponent
+        },
+        {
+          path: 'pools-results',
+          component: PoolsResultsComponent
+        },
+        {
+          path: 'pool-category/:id',
+          component: PoolCategoryComponent
+        },
+        {
+          path: 'pool/:id',
+          component: PoolComponent
+        },
+        {
+          path: 'pool-result/:id',
+          component: PoolResultComponent
+        },
+        {
+          path: 'register-to-pool/:id',
+          component: RegisterToPoolComponent
+        },
+        {
+          path: 'pool-register/:id',
+          component: PoolRegisterComponent
+        },
+      ]
+    ),
+    FormsModule
+  ]
 })
-export class PoolsClientModule { }
+export class PoolsClientModule {
+}
