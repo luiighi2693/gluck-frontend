381a16ff28078cb9b99770d0914532d31196b70f
luiighi2693 <Luis24995059>
2021-08-11 16:29:54 -0400

    qa env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   siteKey: '6LdsgHsbAAAAAE6brkFq24WQ8xqz_ceuE9Wn-IY7',
-  basePath: 'http://localhost:3001',
+  basePath: 'http://206.189.65.211:3001',
   basePath_qa: 'http://206.189.65.211:3001',
   basePath_dev: 'http://localhost:3001',
 };
