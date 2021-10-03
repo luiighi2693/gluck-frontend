2d659dfafdfb23f6110cce9c22de2887324164e9
luiighi2693 <Luis24995059>
2021-10-03 19:56:07 -0400

    fixes finals

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -145,7 +145,7 @@ export class AdminComponent implements OnInit, AfterViewInit {
     if (isProgress) {
       return pools.filter(x => x.result === 'IN PROCESS');
     } else {
-      return pools.filter(x => x.result !== 'IN PROCESS');
+      return pools.filter(x => x.result === 'FINISHED');
     }
   }
 
